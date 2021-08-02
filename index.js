fa2cc641cd3ca9298e5328cd1de43a78101296a8
Girdhar Agarwal <agarwalgirdhar2002@gmail.com>
2021-08-02 20:52:27 +0530

    begineer

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
+function rotate(idname)
+{
+    let id = null;
+    const elem= document.getElementById(idname);
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame,10);
+    function frame() {
+    if (pos>=360) 
+    {
+      clearInterval(id);
+    } 
+    else 
+    {
+      pos=pos+10;
+      elem.style.transform="rotate("+pos+"deg)";
+    }
+  }
+}
 function func()
 {
+    rotate("img1");
+    rotate("img2");
     var r1=Math.floor(Math.random()*6)+1;
     var r2=Math.floor(Math.random()*6)+1;
-    document.getElementsByClassName("img1")[0].setAttribute("src","images/dice"+r1+".png");
-    document.getElementsByClassName("img2")[0].setAttribute("src","images/dice"+r2+".png");
+    document.getElementById("img1").setAttribute("src","images/dice"+r1+".png");
+    document.getElementById("img2").setAttribute("src","images/dice"+r2+".png");
     if(r1==r2)
-    document.getElementsByClassName("result")[0].innerHTML="Tie";
+    document.getElementById("result").innerHTML="Tie";
     else if(r1>r2)
-    document.getElementsByClassName("result")[0].innerHTML="PLAYER 1 WINS";
+    document.getElementById("result").innerHTML="PLAYER 1 WINS";
     else
-    document.getElementsByClassName("result")[0].innerHTML="PLAYER 2 WINS";
+    document.getElementById("result").innerHTML="PLAYER 2 WINS";
 }
 function exitfunc()
 {
-    document.getElementsByClassName("img1")[0].setAttribute("src","images/dice6.png");
-    document.getElementsByClassName("img2")[0].setAttribute("src","images/dice6.png");
-    document.getElementsByClassName("result")[0].innerHTML=" ";
+    document.getElementById("img1").setAttribute("src","images/dice6.png");
+    document.getElementById("img2").setAttribute("src","images/dice6.png");
+    document.getElementById("result").innerHTML=" ";
 }
